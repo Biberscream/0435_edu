7b115e246a7357854dd87ca11cafd4519f97fa4a
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-24 11:35:20 +0300

    Update Main.java

--- task_08/Main.java
+++ task_08/Main.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
+public class Main {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int TC = scan.nextInt();
+        int FT = (9 / 5) * TC +32;
+        System.out.print(FT);
+    }
+}

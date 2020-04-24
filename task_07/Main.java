c59ee6a26108d9ba0cdfcf8d07efc5e5937dffb0
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-24 11:32:36 +0300

    Update Main.java

--- task_07/Main.java
+++ task_07/Main.java
@@ -1,1 +1,10 @@
+import java.util.Scanner;
+public class Main {
 
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int redius = scan.nextInt();
+        double L = 2 * 3.14 * redius;
+        System.out.print(L);
+    }
+}

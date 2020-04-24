7d7824634a051bee837750d96cd643a464ab825a
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-24 11:03:16 +0300

    Create Main.java

---
+++ task_06/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number;
+        int pow = 2;
+        System.out.println(Math.pow(number, pow));
+           }
+}

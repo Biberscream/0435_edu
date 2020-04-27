002d5aa3f21b53c055e60431ca8be1fea4a2e6f9
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 22:29:05 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        /* С помощью цикла for сформируйте строку '123456789' и запишите ее в переменную str */
+        String str = " ";
+        for (int i = 1; i < 10; i++)
+            str = str + i;
+            System.out.println(str);
+    }
+}

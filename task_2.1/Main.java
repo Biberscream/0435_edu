b58d0dac1b4f7675bae3dc821baf14b7fcdb2da6
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 10:12:09 +0300

    Update Main.java

--- task_2.1/Main.java
+++ task_2.1/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 84, 3, 64, 12, 8, 7, 9, 2, 1};
         double sum = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        System.out.println(sum / 10);
+        System.out.println(sum / arr.length);
     }
 }

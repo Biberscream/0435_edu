62577cd1afe04738f57977b25979a74b89c39b92
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 07:26:01 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -1,1 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {5,3,9,4,87,6};
+        int min = nums[0];
+        for (int i = 1; i < 6; i++) {
+            if (min>nums[i]) min = nums[i];
+        }
+        System.out.println(min);
+    }
 

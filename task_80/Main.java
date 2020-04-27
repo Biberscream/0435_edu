70c78d402568e4982919e54599867628b74c23b3
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 07:39:49 +0300

    Update Main.java

--- task_80/Main.java
+++ task_80/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int arr[];
         arr = new int [10];
     }
+
+    // Оснавная идея в том, чтобы сканнер брал значения , которые берутся с клавиатуры и записывал их в массив. Попробую позже

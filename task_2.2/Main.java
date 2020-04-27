67aed82c5da9452880e6c6277d9fcfa1c682fc91
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 10:19:10 +0300

    Update Main.java

--- task_2.2/Main.java
+++ task_2.2/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int[] num = {1, 84, 3, 64, 12, 8, 7, 9, 2, 1};
         int max = 0;
-        for (int i = 1; i < 10; i++) {
-            if (max < num[i])
-                max = num[i];
+        for (int i = 1; i <num.lenght; i++) {
+            if (max < num[i]) max = num[i];
         }
         System.out.print("Max number:  "+max);
     }

9412e189856f684e1b8220a82237be8f18f7f18f
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 22:23:45 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Выведите в консоль столбец чисел от 1 до 50. */
+        int a = 0;
+        for (int i = 1; i < 51; i++)
+            System.out.println(a + i);
+    }
+}

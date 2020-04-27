c8cafbfa5afcf66315cdba2f5eab2ae26a68bd48
Biberscream <64212131+Biberscream@users.noreply.github.com>
2020-04-27 22:45:43 +0300

    Create Main.java

---
+++ task_2.9/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        /* Напишите фрагмент кода, который принимает от пользователя число и,
+        если число четное, то программа выводит на экран число, которое больше введенного с клавиатуры на 10,
+        в противном случае выводит на экран число в 10 раз больше введенного. */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a % 2 == 0 ? a + 10 : a * 10);
+    }
+}
